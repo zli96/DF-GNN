877d1c791f4051fe05abf324a9a2ed834cea39f4
paoxiaode <1435709051@qq.com>
2023-06-24 13:30:15 +0000

    fix csr2ell

--- dgNN/dgNN/src/convert/convert.cpp
+++ dgNN/dgNN/src/convert/convert.cpp
@@ -29,6 +29,22 @@ std::vector<torch::Tensor> csr2csc(torch::Tensor rowptr, torch::Tensor colind,
     return csr2csc_cuda(rowptr, colind, csr_data);
 }
 
+std::vector<std::vector<std::vector<torch::Tensor>>> BucketPartHyb(int num_rows, int num_cols, torch::Tensor indptr,
+                                                                   torch::Tensor indices, int num_col_parts,
+                                                                   std::vector<int> buckets);
+
+std::vector<std::vector<std::vector<torch::Tensor>>> csr2ell(int num_rows, int num_cols, torch::Tensor indptr,
+                                                             torch::Tensor indices, int num_col_parts,
+                                                             std::vector<int> buckets)
+{
+    assert(indptr.dtype() == torch::kInt32);
+    assert(indices.dtype() == torch::kInt32);
+    assert(indptr.device().type() == torch::kCPU);
+    assert(indices.device().type() == torch::kCPU);
+    return BucketPartHyb(
+        num_rows, num_cols, indptr, indices, num_col_parts, buckets);
+}
+
 torch::Tensor coo2csr_cuda(torch::Tensor cooRowInd, int m);
 
 torch::Tensor coo2csr(torch::Tensor cooRowInd, int m)
@@ -42,4 +58,5 @@ PYBIND11_MODULE(format_conversion, m)
     m.doc() = "matrix format transformation";
     m.def("csr2csc", &csr2csc, "csr2csc");
     m.def("coo2csr", &coo2csr, "coo2csr");
+    m.def("csr2ell", &csr2ell, "csr2ell");
 }
