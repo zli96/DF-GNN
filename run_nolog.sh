e39c203cfd53c7410786f5bbea4298fa272f2ba4
paoxiaode <1435709051@qq.com>
2023-07-01 12:00:21 +0000

    add data dir

--- run_nolog.sh
+++ run_nolog.sh
@@ -1,6 +1,7 @@
 read -p "Enter dim(default=64): " dim  
 read -p "Enter heads(default=8): " heads 
 read -p "Enter batch size(default=256): " bs
+read -p "Enter data dir(default=/workspace2/dataset): " data_dir
 
 if [ -z "${dim}" ];then
 	dim=64
@@ -11,8 +12,12 @@ fi
 if [ -z "${bs}" ];then
 	bs=256
 fi
+if [ -z "${data_dir}" ];then
+	data_dir="/workspace2/dataset"
+fi
+
 
 
 python setup.py develop
-python -u dgNN/script/test/test_gf.py --dim $dim --heads $heads --batch-size $bs
+python -u dgNN/script/test/test_gf.py --dim $dim --heads $heads --batch-size $bs --data-dir ${data_dir}
 # python -u dgNN/script/test/test_gf_ell.py --dim $dim --heads $heads --batch-size $bs
