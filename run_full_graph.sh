448243d0c819ac11113d33b82a6b77638ee81390
paoxiaode <1435709051@qq.com>
2023-09-11 06:22:58 +0000

    refine bash format

--- run_full_graph.sh
+++ run_full_graph.sh
@@ -1,15 +1,15 @@
 # read -p "Enter format(default=csr): " format
-read -p "Enter dim(default=64): " dim  
-read -p "Enter heads(default=1): " heads 
+read -p "Enter dim(default=64): " dim
+read -p "Enter heads(default=1): " heads
 read -p "Enter data dir(default=/workspace2/dataset): " data_dir
 
-if [ -z "${dim}" ];then
+if [ -z "${dim}" ]; then
 	dim=64
 fi
-if [ -z "${heads}" ];then
+if [ -z "${heads}" ]; then
 	heads=1
 fi
-if [ -z "${data_dir}" ];then
+if [ -z "${data_dir}" ]; then
 	data_dir="/workspace2/dataset"
 fi
 # if [ -z "${format}" ];then
@@ -34,10 +34,8 @@ python setup.py develop
 # done
 
 num_neighs=(2 4 8 16 32 64 128)
-for format in ${formats[@]};
-do
-for num_neigh in ${num_neighs[@]};
-do
-python -u dgNN/utils/graph_generate.py --format ${format} --num-neigh ${num_neigh} | tee log/day_${day}/gf_constant_degree_${format}_neigh${num_neigh}_${Time}.log
-done
+for format in ${formats[@]}; do
+	for num_neigh in ${num_neighs[@]}; do
+		python -u dgNN/utils/graph_generate.py --format ${format} --num-neigh ${num_neigh} | tee log/day_${day}/gf_constant_degree_${format}_neigh${num_neigh}_${Time}.log
+	done
 done
--- run_subgraph.sh
+++ run_subgraph.sh
@@ -1,48 +1,50 @@
-read -p "Enter dataset(default=ogbg-molhiv, ogbg-molpcba): " dataset
+#!/bin/bash
+
 read -p "Enter dim(default=64): " dim
 
-if [ -z "${dim}" ];then
-    dim=64
+if [ -z "${dim}" ]; then
+    dim=32
 fi
 
-if [ -z "${dataset}" ];then
-	dataset="ogbg-molhiv"
-	# dataset="PATTERN"
-fi
-
+# if [ -z "${dataset}" ];then
+# 	# dataset="ogbg-molhiv"
+# 	dataset="CLUSTER"
+# 	# dataset="PATTERN"
+# fi
 
 day=$(date +%m_%d)
 Time=$(date +%H_%M_%S)
 mkdir log/day_${day}
 mkdir log/ncu/day_${day}
 
-# dataset=PATTERN
-# dataset=CLUSTER
-# dataset=CIFAR10
-# dataset=MNIST
+batch_sizes=(32 64 128 256 512 1024 2048 4096)
+# batch_sizes=(32 2048)
+# batch_sizes=(32)
 
 
+# datasets=(PATTERN CLUSTER MNIST CIFAR10)
 export BLOCK_SIZE=32
-# export BLOCK_SIZE=32
-# batch_sizes=(32 64 128 256 512 1024 2048 4096)
-batch_sizes=(32)
+datasets=(PATTERN)
+
 
 set -e
 python setup.py develop
-for bs in ${batch_sizes[@]};
-do
-    # # run without log
-    python -u dgNN/script/test/test_gf_subgraph.py --batch-size ${bs} --dataset ${dataset} --dim ${dim}  
 
-    # # run with log
-    # python -u dgNN/script/test/test_gf_subgraph.py --batch-size ${bs} --dataset ${dataset} --dim ${dim} | tee log/day_${day}/gf_subgraph_${dataset}_blocksize${BLOCK_SIZE}_dim${dim}_h${heads}_bs${bs}_${Time}.log
+for dataset in ${datasets[@]}; do
+    for bs in ${batch_sizes[@]}; do
+        # # run without log
+        # python -u dgNN/script/test/test_gf_subgraph.py --batch-size ${bs} --dataset ${dataset} --dim ${dim}
 
-    # # debug
-    # CUDA_LAUNCH_BLOCKING=1 python -u dgNN/script/test/test_gf_subgraph.py --batch-size ${bs} --dataset ${dataset} --dim ${dim}
+        # # run with log
+        name=gf_subgraph_${dataset}_blocksize${BLOCK_SIZE}_dim${dim}_bs${bs}_${Time}
+        python -u dgNN/script/test/test_gf_subgraph.py --batch-size ${bs} --dataset ${dataset} --dim ${dim} | tee log/day_${day}/${name}.log
 
-    
+        # # debug
+        # CUDA_LAUNCH_BLOCKING=1 python -u dgNN/script/test/test_gf_subgraph.py --batch-size ${bs} --dataset ${dataset} --dim ${dim}
 
-    # # run ncu profile
-    # name=ncu_gf_subgraph_${dataset}_blocksize${BLOCK_SIZE}_dim${dim}_bs${bs}_${Time}
-    # ncu --set full --import-source yes -c 10 -o log/ncu/day_${day}/${name} -k "fused_forward_kernel_subgraph" python -u dgNN/script/test/test_gf_subgraph.py --batch-size ${bs} --dataset ${dataset} --dim ${dim} --profile  > log/ncu/day_${day}/${name}.log 2>&1 
+        # # run ncu profile
+        # name=ncu_gf_subgraph_no_r2r3_${dataset}_blocksize${BLOCK_SIZE}_dim${dim}_bs${bs}_${Time}
+        # ncu --set full --import-source yes -c 10 -o log/ncu/day_${day}/${name} -k "fused_forward_kernel_subgraph" python -u dgNN/script/test/test_gf_subgraph.py --batch-size ${bs} --dataset ${dataset} --dim ${dim} --profile  > log/ncu/day_${day}/${name}.log 2>&1
+    done
+    wait
 done
