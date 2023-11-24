ec7bedafdf6428bab1bca092b37e81f7b5f6bf82
paoxiaode <1435709051@qq.com>
2023-11-24 03:16:42 +0000

    reconstruct GATconv hyper&&dgNN

--- dgNN/layers/GAT/gatconv_layer_dgNN.py
+++ dgNN/layers/GAT/gatconv_layer_dgNN.py
@@ -1,39 +1,27 @@
 from dgNN.operators.fused_gatconv import GATConvFuse_inference
-from dgNN.utils import Timer
+from dgNN.utils import benchmark
 
 from .gatconv_layer import GATConvDGL
 
 # our gat layer in hyper method
 class GATConv_dgNN(GATConvDGL):
+    def conv(self, row_ptr, col_ind, feat):
+        h = self.W(feat).view(-1, self.out_size, self.num_heads)
+        attn_row = (self.a_l * h).sum(dim=1)
+        attn_col = (self.a_r * h).sum(dim=1)
+        h = h.view(-1, self.num_heads, self.out_size)
+
+        out = GATConvFuse_inference(
+            attn_row, attn_col, row_ptr, col_ind, self.negative_slope, h
+        )
+        return out
+
     def forward(self, params, feat, fuse=False):
         N = len(feat)
         A, row_ptr, col_ind, _, _ = params
         if fuse:
-            for i in range(3):
-                h = self.W(feat).view(-1, self.out_size, self.num_heads)
-                attn_row = (self.a_l * h).sum(dim=1)
-                attn_col = (self.a_r * h).sum(dim=1)
-                h = h.view(-1, self.num_heads, self.out_size)
-
-                out = GATConvFuse_inference(
-                    attn_row, attn_col, row_ptr, col_ind, self.negative_slope, h
-                )
-            with Timer() as t:
-                for i in range(100):
-                    h = self.W(feat).view(-1, self.out_size, self.num_heads)
-                    attn_row = (self.a_l * h).sum(dim=1)
-                    attn_col = (self.a_r * h).sum(dim=1)
-                    h = h.view(-1, self.num_heads, self.out_size)
-                    out = GATConvFuse_inference(
-                        attn_row, attn_col, row_ptr, col_ind, self.negative_slope, h
-                    )
+            out, elapsed_time = benchmark(self.conv, row_ptr, col_ind, feat)
         else:
-            for i in range(3):
-                out = self.forward_nofuse(A, feat)
+            out, elapsed_time = benchmark(self.forward_nofuse, A, feat)
 
-            with Timer() as t:
-                for i in range(100):
-                    out = self.forward_nofuse(A, feat)
-
-        elapsed_time = t.elapsed_secs / 100
         return out.reshape(N, -1), elapsed_time * 1000
--- dgNN/layers/GAT/gatconv_layer_hyper.py
+++ dgNN/layers/GAT/gatconv_layer_hyper.py
@@ -1,52 +1,35 @@
 from dgNN.operators.fused_gatconv import GATConvFuse_inference_hyper
-from dgNN.utils import Timer
+from dgNN.utils import benchmark
 
 from .gatconv_layer import GATConvDGL
 
 
 class GATConv_hyper(GATConvDGL):
+    def conv(self, indptr, indices, rows, smem_consume, feat):
+        h = self.W(feat).view(-1, self.out_size, self.num_heads)
+        attn_row = (self.a_l * h).sum(dim=1)
+        attn_col = (self.a_r * h).sum(dim=1)
+        h = h.view(-1, self.num_heads, self.out_size)
+        out = GATConvFuse_inference_hyper(
+            smem_consume,
+            attn_row,
+            attn_col,
+            indptr,
+            indices,
+            rows,
+            self.negative_slope,
+            h,
+        )
+        return out
+
     def forward(self, params, feat, fuse=False):
         N = len(feat)
         A, indptr, indices, rows, _, smem_consume = params
         if fuse:
-            for i in range(3):
-                h = self.W(feat).view(-1, self.out_size, self.num_heads)
-                attn_row = (self.a_l * h).sum(dim=1)
-                attn_col = (self.a_r * h).sum(dim=1)
-                h = h.view(-1, self.num_heads, self.out_size)
-                out = GATConvFuse_inference_hyper(
-                    smem_consume,
-                    attn_row,
-                    attn_col,
-                    indptr,
-                    indices,
-                    rows,
-                    self.negative_slope,
-                    h,
-                )
-            with Timer() as t:
-                for i in range(100):
-                    h = self.W(feat).view(-1, self.out_size, self.num_heads)
-                    attn_row = (self.a_l * h).sum(dim=1)
-                    attn_col = (self.a_r * h).sum(dim=1)
-                    h = h.view(-1, self.num_heads, self.out_size)
-                    out = GATConvFuse_inference_hyper(
-                        smem_consume,
-                        attn_row,
-                        attn_col,
-                        indptr,
-                        indices,
-                        rows,
-                        self.negative_slope,
-                        h,
-                    )
+            out, elapsed_time = benchmark(
+                self.conv, indptr, indices, rows, smem_consume, feat
+            )
         else:
-            for i in range(3):
-                out = self.forward_nofuse(A, feat)
+            out, elapsed_time = benchmark(self.forward_nofuse, A, feat)
 
-            with Timer() as t:
-                for i in range(100):
-                    out = self.forward_nofuse(A, feat)
-
-        elapsed_time = t.elapsed_secs / 100
         return out.reshape(N, -1), elapsed_time * 1000
