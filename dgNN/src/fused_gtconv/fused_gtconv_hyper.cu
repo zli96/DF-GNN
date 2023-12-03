7bc31fd9c16e0fe61c7f0355606b5a0890debcf6
paoxiaode <1435709051@qq.com>
2023-12-03 03:33:31 +0000

    update gt hyper method (CWM)

--- dgNN/src/fused_gtconv/fused_gtconv_hyper.cu
+++ dgNN/src/fused_gtconv/fused_gtconv_hyper.cu
@@ -276,7 +276,7 @@ __global__ void fused_gt_hyper_inference(const int m, const int h, const int f,
     }
 
     // compute the output
-    int loop_f = (f + WARP_SIZE - 1) / WARP_SIZE;
+    // int loop_f = (f + WARP_SIZE - 1) / WARP_SIZE;
     // int loop_f = (f + WARP_SIZE* 2 - 1) / WARP_SIZE/2;
     // float2 * out_feat2 = reinterpret_cast<float2*>(out_feat);
 
@@ -298,21 +298,152 @@ __global__ void fused_gt_hyper_inference(const int m, const int h, const int f,
     //     out_feat2[(curr_node * hf + hid * f)/2 + pid] =
     //         (expAll != 0) ? acc / expAll : make_float2(0,0);
     // }
-
-    for (int i = 0; i < loop_f; i += 1) {
+    for (int i = tidx; i < f; i += 64) {
       DType acc = 0;
-      int pid = tidx + (i << 5);
+      DType acc2 = 0;
+      // int pid = tidx + (i << 5);
       for (int j = 0; j < num_edge; j++) {
         int cid = indices[edge_lb + j];
         DType attn_val = neigh_nodes_weight_off[j];
-        if (pid < f)
-          acc += attn_val * V[cid * hf + hid * f + pid];
+        acc += attn_val * V[cid * hf + hid * f + i];
+        if (i + 32 < f) {
+          acc2 += attn_val * V[cid * hf + hid * f + i + 32];
+        }
       }
       // handle the node with no neighbor
-      if (pid < f)
-        out_feat[curr_node * hf + hid * f + pid] =
-            (expAll != 0) ? acc / expAll : 0;
+      out_feat[curr_node * hf + hid * f + i] = (expAll != 0) ? acc / expAll : 0;
+      if (i + 32 < f) {
+        out_feat[curr_node * hf + hid * f + i + 32] =
+            (expAll != 0) ? acc2 / expAll : 0;
+      }
     }
+  }
+}
+
+template <typename DType>
+__global__ void fused_gt_hyper_inference_small_f(
+    const int m, const int h, const int f, const int *row, const int *indptr,
+    const int *indices, const DType *val, const DType *Q, const DType *K,
+    const DType *V, DType *out_feat) {
+  // launch dim (32, 8) * (num_nodes/8, 1)
+
+  const int bidx = blockIdx.x;
+  const int hid = blockIdx.y;
+  const int tidx = threadIdx.x;
+  const int tidy = threadIdx.y;
+
+  // the node bound of this block
+  const int blockSize = blockDim.y;
+  const int blk_node_lb = blockSize * bidx;
+  const int blk_node_hb = MIN(blk_node_lb + blockSize, m);
+
+  // the edge bound of this block
+  const int blk_edge_lb = indptr[blk_node_lb];
+  const int blk_edge_hb = indptr[blk_node_hb];
+
+  // the num of edges in this block
+  const int blk_num_edge = blk_edge_hb - blk_edge_lb;
+
+  // init smem
+  extern __shared__ DType smem[];
+  DType *neigh_nodes_weight = smem; // [8, f]
+
+  // SDDMM, edge parallel
+  int nnz_per_warp = (blk_num_edge + blockSize - 1) / blockSize;
+
+  const int *rowoff = row + blk_edge_lb;
+  const int *indicesoff = indices + blk_edge_lb;
+  const DType *valoff = val + blk_edge_lb;
+
+  int src;
+  int dst;
+  for (int i = 0; i < nnz_per_warp; i++) {
+    int curr_edge = tidy * nnz_per_warp + i;
+    // edge bound for curr block
+    if (curr_edge < blk_num_edge) {
+      src = __ldg(rowoff + curr_edge);
+      dst = __ldg(indicesoff + curr_edge);
+
+      // // the Q feature of row node
+      const DType *Qoff = Q + src * f * h + hid * f;
+      // the K feature of col node
+      const DType *Koff = K + dst * f * h + hid * f;
+
+      DType att_val = 0;
+      if (tidx < f) {
+        att_val += Qoff[tidx] * Koff[tidx];
+      }
+#pragma unroll
+      for (int offset = 16; offset > 0; offset /= 2)
+        att_val += __shfl_down_sync(full_mask, att_val, offset);
+      if (tidx == 0) {
+        neigh_nodes_weight[curr_edge] = att_val * valoff[curr_edge];
+      }
+    }
+  }
+  __syncthreads();
+
+  // Softmax+SPMM, node parallel
+  int curr_node = blk_node_lb + tidy;
+  if (curr_node < blk_node_hb) {
+    const int edge_lb = indptr[curr_node];
+    const int edge_hb = indptr[curr_node + 1];
+    const int num_edge = edge_hb - edge_lb;
+
+    DType weightMax = -1e38;
+    const int hf = h * f;
+    // const int hfid = hid * f + tidx;
+
+    DType *neigh_nodes_weight_off =
+        neigh_nodes_weight + (edge_lb - blk_edge_lb);
+
+    int loop = (num_edge + WARP_SIZE - 1) / WARP_SIZE;
+    for (int j = 0; j < loop; j++) {
+      DType weight = -1e38;
+      int pid = tidx + (j << 5);
+      if (pid < num_edge) {
+        weight = neigh_nodes_weight_off[pid];
+      }
+      __syncwarp();
+#pragma unroll
+      for (int stride = 16; stride > 0; stride >>= 1) {
+        weight = max(__shfl_xor_sync(0xffffffff, weight, stride, 32), weight);
+      }
+      __syncwarp();
+      weightMax = MAX(weight, weightMax);
+    }
+
+    // compute the sum of exp
+    DType expAll = 0;
+    for (int j = 0; j < loop; j++) {
+      int pid = tidx + (j << 5); // node need to process in loop j
+      DType exptmp = 0;
+      if (pid < num_edge) {
+        DType weight = neigh_nodes_weight_off[pid];
+        exptmp = exp(weight - weightMax);
+        neigh_nodes_weight_off[pid] = exptmp;
+      }
+      __syncwarp();
+#pragma unroll
+      for (int stride = 16; stride > 0; stride >>= 1) {
+        exptmp += __shfl_xor_sync(0xffffffff, exptmp, stride, 32);
+      }
+      __syncwarp();
+      expAll += exptmp;
+    }
+
+    // compute the output
+    DType acc = 0;
+    for (int j = 0; j < num_edge; j++) {
+      int cid = indices[edge_lb + j];
+      DType attn_val = neigh_nodes_weight_off[j];
+      if (tidx < f)
+        acc += attn_val * V[cid * hf + hid * f + tidx];
+    }
+    // handle the node with no neighbor
+    if (tidx < f)
+      out_feat[curr_node * hf + hid * f + tidx] =
+          (expAll != 0) ? acc / expAll : 0;
   }
 }
 
@@ -701,11 +832,19 @@ gt_hyper_inference_cuda(torch::Tensor indptr, torch::Tensor indices,
   const dim3 nthrs(ntx, nty);
   const int smem_size = smem_consume * sizeof(float);
 
-  CUDA_KERNEL_CALL((fused_gt_hyper_inference<float>), nblks, nthrs, smem_size,
-                   m, h, f, rows.data_ptr<int>(), indptr.data_ptr<int>(),
-                   indices.data_ptr<int>(), val.data_ptr<float>(),
-                   Q.data_ptr<float>(), K.data_ptr<float>(),
-                   V.data_ptr<float>(), out_feat.data_ptr<float>());
+  if (f <= 32) {
+    CUDA_KERNEL_CALL(
+        (fused_gt_hyper_inference_small_f<float>), nblks, nthrs, smem_size, m,
+        h, f, rows.data_ptr<int>(), indptr.data_ptr<int>(),
+        indices.data_ptr<int>(), val.data_ptr<float>(), Q.data_ptr<float>(),
+        K.data_ptr<float>(), V.data_ptr<float>(), out_feat.data_ptr<float>());
+  } else {
+    CUDA_KERNEL_CALL((fused_gt_hyper_inference<float>), nblks, nthrs, smem_size,
+                     m, h, f, rows.data_ptr<int>(), indptr.data_ptr<int>(),
+                     indices.data_ptr<int>(), val.data_ptr<float>(),
+                     Q.data_ptr<float>(), K.data_ptr<float>(),
+                     V.data_ptr<float>(), out_feat.data_ptr<float>());
+  }
 
   return {out_feat};
 }
